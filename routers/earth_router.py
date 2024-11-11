0da9079fce6502676923b805cd25143f3c838d4e
emiledelmas <emile.delmas@gmail.com>
2024-11-11 14:43:13 +0000

    add satellites to json file instead of hardcoded

--- routers/earth_router.py
+++ routers/earth_router.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+import json
 
 from config.constants import SATELLITE_FUNCTION_DISASTER_IMAGING
 from controllers.create_header import create_header
@@ -6,6 +7,11 @@ from middleware.header import check_headers
 import base64
 
 router = Blueprint('main', __name__)
+
+def read_satellites_from_json():
+    with open('satellites.json', 'r') as json_file:
+        data = json.load(json_file)
+        return data['satellites']
 
 @router.route('/v1/create-header', methods=['POST'])
 def create_custom_headers():
@@ -17,7 +23,8 @@ def get_satellites():
     if middleware is not True:
         return middleware
 
-    return {"satellites": [{"location": "Valencia", "ip": "2001:0000:130F:0000:0000:09C0:876A:130B", "function": "disaster-imaging"}, {"location": "Madrid", "ip": "2001:0000:130F:0000:0000:09C0:876A:130C", "function": "disaster-imaging"}, {"location": "Barcelona", "ip": "2001:0000:130F:0000:0000:09C0:876A:130D", "function": "disaster-imaging"}, {"location": "Sevilla", "ip": "2001:0000:130F:0000:0000:09C0:876A:130E", "function": "disaster-imaging"}, {"location": "Zaragoza", "ip": "2001:0000:130F:0000:0000:09C0:876A:130F", "function": "disaster-imaging"}, {"location": "Málaga", "ip": "2001:0000:130F:0000:0000:09C0:876A:1310", "function": "disaster-imaging"}, {"location": "Murcia", "ip": "2001:0000:130F:0000:0000:09C0:876A:1311", "function": "disaster-imaging"}, {"location": "Palma de Mallorca", "ip": "2001:0000:130F:0000:0000:09C0:876A:1312", "function": "disaster-imaging"}, {"location": "Las Palmas de Gran Canaria", "ip": "2001:0000:130F:0000:0000:09C0:876A:1313", "function": "disaster-imaging"}, {"location": "Bilbao", "ip": "2001:0000:130F:0000:0000:09C0:876A:1314", "function": "disaster-imaging"}]}
+    satellites = read_satellites_from_json()
+    return {"satellites": satellites}
 
 @router.route('/v1/satellites/<string:ip>/images', methods=['POST'])
 def capture_image(ip):
@@ -25,28 +32,18 @@ def capture_image(ip):
     # if middleware is not True:
     #     return middleware
 
-    satellites = {
-        "satellites": [
-        {"location": "Valencia", "ip": "2001:0000:130F:0000:0000:09C0:876A:130B", "function": "disaster-imaging"},
-        {"location": "Madrid", "ip": "2001:0000:130F:0000:0000:09C0:876A:130C", "function": "disaster-imaging"},
-        {"location": "Barcelona", "ip": "2001:0000:130F:0000:0000:09C0:876A:130D", "function": "disaster-imaging"},
-        {"location": "Sevilla", "ip": "2001:0000:130F:0000:0000:09C0:876A:130E", "function": "disaster-imaging"},
-        {"location": "Zaragoza", "ip": "2001:0000:130F:0000:0000:09C0:876A:130F", "function": "disaster-imaging"},
-        {"location": "Málaga", "ip": "2001:0000:130F:0000:0000:09C0:876A:1310", "function": "disaster-imaging"},
-        {"location": "Murcia", "ip": "2001:0000:130F:0000:0000:09C0:876A:1311", "function": "disaster-imaging"},
-        {"location": "Palma de Mallorca", "ip": "2001:0000:130F:0000:0000:09C0:876A:1312", "function": "disaster-imaging"},
-        {"location": "Las Palmas de Gran Canaria", "ip": "2001:0000:130F:0000:0000:09C0:876A:1313", "function": "disaster-imaging"},
-        {"location": "Bilbao", "ip": "2001:0000:130F:0000:0000:09C0:876A:1314", "function": "whale-tracking"}
-        ]
-    }
+    satellites = read_satellites_from_json()
 
-    if ip not in [satellite["ip"] for satellite in satellites["satellites"]]:
+    if ip not in [sat["ip"] for sat in satellites]:
         return {"error": "Satellite not found"}, 404
 
-    satellite = [satellite for satellite in satellites["satellites"] if satellite["ip"] == ip][0]
+    satellite = next((sat for sat in satellites if sat["ip"] == ip), None)
     if satellite["function"] != SATELLITE_FUNCTION_DISASTER_IMAGING:
-        return {"error": "Satellite can not take images", "status": "failure", "status_code": 400}, 400
-
+        return {
+            "error": "Satellite can not take images",
+            "status": "failure",
+            "status_code": 400
+        }, 400
 
     with open("development/mar-menor.jpg", "rb") as image_file:
         base64_bytes = base64.b64encode(image_file.read())
